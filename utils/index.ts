99d3462a1caaaf2c847728adcf8c8349f6bf8090
Surajit Shil <38415007+surajit50@users.noreply.github.com>
2026-04-28 09:34:52 +0530

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -1,16 +1,10 @@
-// Function to convert image to base64
-export const getBase64FromUrl = async (url: string) => {
-  try {
-    const response = await fetch(url);
-    const blob = await response.blob();
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onloadend = () => resolve(reader.result);
-      reader.onerror = reject;
-      reader.readAsDataURL(blob);
-    });
-  } catch (error) {
-    console.error("Error loading image:", error);
-    return null;
-  }
+export const getBase64FromUrl = async (url: string): Promise<string> => {
+  const response = await fetch(url);
+  const blob = await response.blob();
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onloadend = () => resolve(reader.result as string);
+    reader.onerror = reject;
+    reader.readAsDataURL(blob);
+  });
 };
