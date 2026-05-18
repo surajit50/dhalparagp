8c43431a1b458432ef99a4d2091d0aaac26ff8ab
Surajit Shil <no3dhalparagp@gmail.com>
2026-05-18 17:10:15 +0530

    fffa

--- app/layout.tsx
+++ app/layout.tsx
@@ -218,7 +218,7 @@ export default async function RootLayout({ children }: Props) {
       <body>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
