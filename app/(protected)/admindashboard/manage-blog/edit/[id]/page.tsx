b834a2ecb334fb5d4260d7de7e280149283888b6
Surajit Shil <no3dhalparagp@gmail.com>
2026-06-04 16:39:57 +0530

    Fafafa

--- app/(protected)/admindashboard/manage-blog/edit/[id]/page.tsx
+++ app/(protected)/admindashboard/manage-blog/edit/[id]/page.tsx
@@ -2,8 +2,9 @@ import { getBlogPostById } from "@/action/blog-actions";
 import EditBlogForm from "./EditBlogForm";
 import { notFound } from "next/navigation";
 
-export default async function EditBlogPage({ params }: { params: { id: string } }) {
-  const result = await getBlogPostById(params.id);
+export default async function EditBlogPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  const result = await getBlogPostById(id);
 
   if (result.error || !result.success) {
     return notFound();
