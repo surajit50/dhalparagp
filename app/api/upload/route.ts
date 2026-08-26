9fc6d24486fc8f7cb02a0c8e55c8200957036f1f
Surajit Shil <38415007+surajit50@users.noreply.github.com>
2026-08-26 20:48:55 +0530

    Update route.ts

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -17,9 +17,9 @@ export async function OPTIONS() {
 
 // Configure Cloudinary
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME2,
+  api_key: process.env.CLOUDINARY_API_KEY2,
+  api_secret: process.env.CLOUDINARY_API_SECRET2,
 });
 
 /**
