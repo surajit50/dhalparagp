ea57583e85b5d1450ebcd0f7dd6e4c6557122b08
Surajit Shil <surajit.shil.shil@gmail.com>
2026-09-17 11:14:54 +0530

    Fafaf

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,8 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+export { formatDate } from "./date";
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
